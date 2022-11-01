ae441afe14be23fb5ddc3e70778eb9becced9993
luxkatana <taseen.bibi@gmail.com>
2022-11-01 20:33:12 +0100

    modified:   main.py 	modified:   requirements.txt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import discord, aiomysql
+from os import environ
+from dotenv import load_dotenv
 from discord.ext import commands
-
+load_dotenv()
 bot = commands.Bot(command_prefix="./", intents=discord.Intents.all(),
                    activity=discord.Game("Tests!!"),
                    debug_guilds=[941803156633956362])
@@ -9,13 +11,13 @@ bot.load_extension("cogs")
 @bot.event
 async def on_ready() -> None:
     bot.pool = await aiomysql.create_pool(
-        host="localhost",
-        port=3306,
-        user="root",
-        password="inchem2009",
-        db="testdb"
+        host=environ["HOST"],
+        port=environ["PORT"],
+        user=environ["USER"],
+        password=environ["PASSWORD"],
+        db=environ["DB"]
     )
     print(f"Logged in as {bot.user}")
 
 
-bot.run("MTAxNDk1ODIzNTUxOTgzMjIwNg.G583j3.SLSxBkRPCmuK71FNrZhO8TG67_x5b6-iTNMKvI")
+bot.run(environ["BOT_TOKEN"])
