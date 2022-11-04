63b6a7ef5397d09da591bf05ff6fe9b9a91696e9
luxkatana <taseen.bibi@gmail.com>
2022-11-04 20:32:42 +0100

    added leaderboard for invitelogger

--- cogs.py
+++ cogs.py
@@ -1,4 +1,4 @@
-import discord, aiomysql
+import discord, aiomysql, random
 
 from discord.ext import commands
 class Utils(commands.Cog):
@@ -72,5 +72,61 @@ class Utils(commands.Cog):
             await message.reply(info["respond"])
 
         await self.bot.process_commands(message)
+    @commands.slash_command(name="invitetracker", description="Shows how must people  you invited before")
+    async def invitetracker(self, ctx: discord.ApplicationContext) -> None:
+        if ctx.guild.invites_disabled: # if invites disabled
+            embed = discord.Embed(title="RIP", description="Invites are disabled in this server...", colour=discord.Color.red())
+            await ctx.respond(embed=embed, ephemeral=True)
+        else:
+            invites = await ctx.guild.invites()
+            inv_count = 0
+            for invite in invites:
+                if invite.inviter == ctx.author:
+                    inv_count += invite.uses
+            embed = discord.Embed(title="Total Invites", description=f"You invited in total **{inv_count}** people.", colour=discord.Color.green())
+            await ctx.respond(embed=embed)
+    @commands.slash_command(name="invitetracker-leaderboard", description="Leaderboard of invites")
+    async def invitetracker_leaderboard(self, ctx: discord.ApplicationContext) -> None:
+        async def remove(array: list[discord.Invite]) -> list:
+            highest = None
+            number_buffer = 0
+            for inv in array:
+                print(inv.uses)
+                if inv.uses > number_buffer:
+                    highest = inv
+                    number_buffer = highest.uses
+                    print(highest, number_buffer, sep="\n")
+                elif inv.uses == 0:
+                    continue
+            del array[array.index(highest)]
+            return [array, highest]
+        if ctx.guild.invites_disabled:
+            await ctx.respond(embed=discord.Embed(title="Invites disabled", description="Invites are disabled in this server...", colour=discord.Color.red()))
+        else:
+            numbers = [random.randint(0, 10) for _ in range(0, 3)]
+            hint = await ctx.respond(embed=discord.Embed(title="Did you know...", description="That {} + {} = {}? ".format(numbers[0], numbers[1], numbers[0] + numbers[1]),
+                                                         fields=
+                                                        [
+                                                            discord.EmbedField(name="Waiting", value="Waiting for response... Be patient", inline=False)
+                                                        ]))
+            invites = await ctx.guild.invites()
+            high_to_low = []
+            invites = list(filter(lambda j: j.uses != 0, invites))
+            print(invites)
+
+            while invites != []:
+                new, high = await remove(invites)
+                if 0 in [new, high]:
+                    break
+                invites = new.copy()
+                high_to_low.append(high)
+            embed = discord.Embed(title="Invites Leaderboard", color=discord.Color.green())
+            for i in range(0, 6): # get 5
+                try:
+                    target = high_to_low[i]
+                except:
+                    break
+                embed.add_field(name=f"{i + 1}st - {target.inviter}", value=f"**{target.uses}** invites", inline=False)
+            await hint.edit_original_response(embed=embed)
 def setup(bot: commands.Bot) -> None:
     bot.add_cog(Utils(bot))
